fab4b9f24eef5c6265ed6a9c80faff9b3d0871e7
Nicky0230 <Bernarda3008@gmail.com>
2018-04-05 09:07:49 -0400

    Seccion 13 terminada

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -7,12 +7,14 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesaComponent } from './promesa/promesa.component';
+import { LoginGuardGuard } from '../services/service.index';
 
 
 
 const pagesRoutes: Routes = [
     {   path: '',
     component: PagesComponent,
+    canActivate: [ LoginGuardGuard],
     children:  [
       { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
       { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' } },

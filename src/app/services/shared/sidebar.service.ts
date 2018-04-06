37c616afa90cb8f3a37fb814968aa9d4d070772b
Nicky0230 <Bernarda3008@gmail.com>
2018-04-06 15:33:49 -0400

    Seccion 15 empezada

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -15,6 +15,15 @@ export class SidebarService {
         { titulo: 'Rxjs', url: '/Rxjs'},
 
       ]
+    },
+    {
+      titulo: 'Mantenimientos',
+      icono: 'mdi mdi-folder-lock-open',
+      submenu: [
+        { titulo: 'Usuarios', url: '/usuarios' },
+        { titulo: 'Hospitales', url: '/hospitales'},
+        { titulo: 'Medicos', url: '/medicos'}
+      ]
     }
   ];
 

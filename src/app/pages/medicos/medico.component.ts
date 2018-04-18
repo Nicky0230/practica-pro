d29cc88c069bab645e4d0170ac99f5ff137ef481
Nicky0230 <Bernarda3008@gmail.com>
2018-04-18 16:52:30 -0400

    Seccion 16 avaanzada

--- src/app/pages/medicos/medico.component.ts
+++ src/app/pages/medicos/medico.component.ts
@@ -1,4 +1,11 @@
+import { Router } from '@angular/router';
+import { Hospital } from './../../models/hospital.model';
+import { MedicoService } from './../../services/service.index';
+import { Medico } from './../../models/medico.model';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HospitalService } from '../../services/service.index';
+
 
 @Component({
   selector: 'app-medico',
@@ -7,9 +14,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoComponent implements OnInit {
 
-  constructor() { }
+  hospitales: Hospital[] = [];
+  medico: Medico = new Medico('', '', '', '');
+  hospital: Hospital = new Hospital('');
+
+  constructor(
+    public _medicoService: MedicoService,
+    public _hospitalService: HospitalService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+
+    this._hospitalService.cargarHospitales()
+                .subscribe( hospitales => this.hospitales = hospitales );
+  }
+
+  guardarMedico( f: NgForm) {
+
+    console.log( f.valid);
+    console.log( f.value);
+
+    if (f.invalid) {
+      return;
+    }
+
+    this._medicoService.guardarMedico( this.medico )
+              .subscribe ( medico => {
+
+                this.medico._id = medico._id;
+
+                this.router.navigate(['/medico', medico._id]);
+
+              });
+
+  }
+
+  cambioHospital( id: string ) {
+
+    this._hospitalService.obtenerHospital( id )
+            .subscribe( hospital => this.hospital = hospital);
+
   }
 
 }
--- src/app/pages/medicos/medicos.component.ts
+++ src/app/pages/medicos/medicos.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Medico } from './../../models/medico.model';
+import { Component, OnInit, } from '@angular/core';
+import { MedicoService } from '../../services/service.index';
+import { URL_SERVICIOS } from '../../config/config';
 @Component({
   selector: 'app-medicos',
   templateUrl: './medicos.component.html',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicosComponent implements OnInit {
 
-  constructor() { }
+  medicos: Medico[] = [];
+
+  constructor(
+    public _medicoService: MedicoService
+  ) { }
 
   ngOnInit() {
+    this.cargarMedicos();
   }
+
+  cargarMedicos() {
+    this._medicoService.cargarMedicos()
+              .subscribe( medicos => this.medicos = medicos );
+  }
+
+  buscarMedico( termino: string ) {
+    if (termino.length <= 0) {
+      this.cargarMedicos();
+      return;
+    }
+    this._medicoService.buscarMedico( termino )
+            .subscribe( medicos => this.medicos = medicos );
+  }
+
+  borrarMedico(medico: Medico) {
+
+    this._medicoService.borrarMedico( medico._id )
+                      .subscribe( () => this.cargarMedicos() );
+
+  }
+
 
 }
